a1a5c903ab0adc5fa5e272ce5e48ca59d1af7bcb
Станислав Шмаков <naitanex@mail.ru>
2021-04-02 17:28:29 +0300

    -file hash generator

--- another.py
+++ another.py
@@ -1,36 +1,18 @@
-from selenium import webdriver
-import time
+from shutil import copy
+import string
+import secrets
+import random
 
-try:
-    link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
 
-    # Rод, который заполняет обязательные поля
-    input1 = browser.find_element_by_css_selector(".first_block .first")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element_by_css_selector(".first_block .second")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element_by_css_selector(".first_block .third")
-    input3.send_keys("ivan_petrov753@yandex.ru")
-    # Отправляем заполненную форму
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
+def generate_random(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    crypt = ''.join(secrets.choice(
+        letters_and_digits) for i in range(length))
+    return crypt
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+for i in range(1, 100):
+    s = random.randint(10000, 200000)
+    chp = open("C:/1/text.txt", "w")
+    chp.write(generate_random(s))
+    copy("C:\\1\\text.txt", "C:\\1\\text" + str(i) + ".txt")

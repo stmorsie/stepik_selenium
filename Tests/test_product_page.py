84fdd68178cac12b657e0957339b162bf6c48863
Станислав Шмаков <naitanex@mail.ru>
2021-04-21 12:27:12 +0300

    -need review

--- Tests/test_product_page.py
+++ Tests/test_product_page.py
@@ -10,7 +10,7 @@ import pytest
 bLink = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
-#pytest.mark.need_review
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 def test_guest_can_add_product_to_basket(browser, link):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
